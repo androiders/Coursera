a592eef17a6ab65ed48da17a24467a73ab91d6fa
Anders Folkesson <anders.folkesson@cybercom.com>
2014-10-24 10:25:11 +0200

    Modern Art UI lab done

--- ModernArtGui/gen/com/coursera/modernartgui/R.java
+++ ModernArtGui/gen/com/coursera/modernartgui/R.java
@@ -24,21 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int color_slider=0x7f070005;
-        public static final int sq1=0x7f070000;
-        public static final int sq2=0x7f070001;
-        public static final int sq3=0x7f070002;
-        public static final int sq4=0x7f070003;
-        public static final int sq5=0x7f070004;
+        public static final int color_slider=0x7f080005;
+        public static final int more_info=0x7f080006;
+        public static final int sq1=0x7f080000;
+        public static final int sq2=0x7f080001;
+        public static final int sq3=0x7f080002;
+        public static final int sq4=0x7f080003;
+        public static final int sq5=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main_menu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int fake_string=0x7f050003;
         public static final int hejhej=0x7f050002;
         public static final int hello_world=0x7f050001;
+        public static final int more_info=0x7f050004;
+        public static final int not_now=0x7f050005;
+        public static final int visit_moma=0x7f050006;
+        public static final int visit_moma_info=0x7f050007;
     }
     public static final class style {
         /** 
--- ModernArtGui/src/com/coursera/modernartgui/MainActivity.java
+++ ModernArtGui/src/com/coursera/modernartgui/MainActivity.java
@@ -1,23 +1,25 @@
 package com.coursera.modernartgui;
 
-import android.R.drawable;
+import com.coursera.modernartgui.ArtDialogFragment.ArtDialogListener;
+
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnSeekBarChangeListener {
+public class MainActivity extends Activity implements OnSeekBarChangeListener, ArtDialogListener {
 
+	private String MOMA_URL = "http://www.moma.com";
+	
 	//refrences to text views for changing colors
 	Button sq1;
 	Button sq2;
@@ -25,7 +27,13 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 	Button sq4;
 	Button sq5;
 	
-	ColorMatrix mColorMatrix;
+	private String COLOR_1_STR = "c1";
+	private String COLOR_2_STR = "c2";
+	private String COLOR_3_STR = "c3";
+	private String COLOR_4_STR = "c4";
+	private String PROGR_STR   = "prgress";
+	
+	int mLastProgress;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +48,41 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 		
 		SeekBar colorSlider = (SeekBar)findViewById(R.id.color_slider);
 		colorSlider.setOnSeekBarChangeListener(this);
+
+		mLastProgress = 0;
 		
-		float colors[] = 
-			{	1,0.5f,0.8f,1,1,
-				1,0.5f,0.3f,1,1,
-				1,0.5f,0.8f,1,1,
-				1,00.5f,0.6f,1,1	};
+		//restore colors and slider progress from savedinstance!!
+		if(savedInstanceState != null)
+		{
+			int c1 = savedInstanceState.getInt(COLOR_1_STR);
+			int c2 = savedInstanceState.getInt(COLOR_2_STR);
+			int c3 = savedInstanceState.getInt(COLOR_3_STR);
+			int c4 = savedInstanceState.getInt(COLOR_4_STR);
+
+			mLastProgress = savedInstanceState.getInt(PROGR_STR);
+			
+			sq1.setBackgroundColor(c1);
+			sq2.setBackgroundColor(c2);
+			sq3.setBackgroundColor(c3);
+			sq4.setBackgroundColor(c4);
+		}
 		
-		mColorMatrix = new ColorMatrix(colors);
+	}
+	
+	@Override
+	protected void onSaveInstanceState (Bundle outState)
+	{
+		//NOTE the illogical order on colors
+		int c1 = getColor1();
+		int c2 = getColor2();
+		int c3 = getColor3();
+		int c5 = getColor5();
+		
+		outState.putInt(COLOR_1_STR,c1);
+		outState.putInt(COLOR_2_STR,c2);
+		outState.putInt(COLOR_3_STR,c3);
+		outState.putInt(COLOR_4_STR,c5);
+		outState.putInt(PROGR_STR,mLastProgress);
 	}
 
 	@Override
@@ -80,11 +115,40 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 		super.onStop();
 	}
 
+	//Create the options menu here!
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_menu, menu);
+	    return true;
+	}
+	
+	private void showDialog()
+	{
+		 DialogFragment newFragment = new ArtDialogFragment();
+		 newFragment.show(getFragmentManager(), "");
+	}
+	
+	
+	//check for clicks in the menu
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.more_info:
+	        	Log.i("Main","Menu item clicked");
+	        	showDialog();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
 	@Override
 	public void onProgressChanged(SeekBar seekBar, int progress,
 			boolean fromUser) {
 		// TODO Auto-generated method stub
-		Log.i("mAIN","color slider changed "+ progress);
+		Log.i("Main","color slider changed "+ progress);
 		updateColors(progress);
 		
 	}
@@ -101,20 +165,95 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
 		
 	}
 	
+	
+	private int getColor1(){
+		return ((ColorDrawable)sq1.getBackground()).getColor();
+	}
+	
+	private int getColor2(){
+		return ((ColorDrawable)sq2.getBackground()).getColor();
+	}
+	
+	private int getColor3(){
+		return ((ColorDrawable)sq3.getBackground()).getColor();
+	}
+
+	private int getColor5(){
+		return ((ColorDrawable)sq5.getBackground()).getColor();
+	}
+	
+	
 	private void updateColors(int progress) {
 		
-		int c1 = ((ColorDrawable)sq1.getBackground()).getColor();
+		int lProg = (progress - mLastProgress)*2;
+		mLastProgress = progress;
+		
+		//get the colors from the ui buttons
+		int c1 = getColor1();
+		int c2 = getColor2();
+		int c3 = getColor3();
+		int c5 = getColor5();
+		
+		//this is where the magic happens!
+		//bit operations to change colors...
 		
 		//change c1 in red color range
 		//mask out  red component
-		int red = (c1 >> 16) & 0xFF;
-		red -= progress;
+		int red = (c3 >> 16) & 0xFF;
+		red += lProg;
 		red = red << 16;
 		//clear red component of c1
-		c1 &= 0xFF00FFFF;
-		//st new red component
-		c1 |= (red & 0x00FF0000);
+		c3 &= 0xFF00FFFF;
+		//set new red component
+		c3 |= (red & 0x00FF0000);
+		sq3.setBackgroundColor(c3);
+		
+		//change c2 in blue color range
+		//mask out  red component
+		int blue =  c2 & 0xFF;
+		blue += lProg;
+		//clear red component of c2
+		c2 &= 0xFFFFFF00;
+		//set new red component
+		c2 |= (blue & 0x000000FF);
+		sq2.setBackgroundColor(c2);
 
+		//change c3 in green color range
+		//mask out  green component
+		int green = (c1 >> 8) & 0xFF;
+		green += lProg ;
+		green = green << 8;
+		//clear red component of c3
+		c1 &= 0xFFFF00FF;
+		//set new red component
+		c1 |= (green & 0x0000FF00);
 		sq1.setBackgroundColor(c1);
+
+		//change c4 in some other color range
+		//mask out some component
+		int some = (c5 >> 16) & 0xFF;
+		some += lProg;
+		some = some << 16;
+		//clear some component of c4
+		c5 &= 0xFF00FFFF;
+		//set new  component
+		c5 |= (some & 0x00FF0000);
+		sq5.setBackgroundColor(c5);
+				
+	}
+
+	@Override
+	public void onDialogPositiveClick(DialogFragment dialog) {
+		Log.i("main","Dialog positively clicked");
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(MOMA_URL));
+		startActivity(i);
+		
+	}
+
+	@Override
+	public void onDialogNegativeClick(DialogFragment dialog) {
+		Log.i("main","Dialog negatively clicked");
+
 	}
 }
