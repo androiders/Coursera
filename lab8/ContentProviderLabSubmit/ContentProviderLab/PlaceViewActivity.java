5d6c54cd8510edd23df2b4efa9b32dc702498ca4
Anders Folkesson <anders.folkesson@cybercom.com>
2014-11-21 07:36:56 +0100

    finished lab8

--- lab8/ContentProviderLabSubmit/ContentProviderLab/PlaceViewActivity.java
+++ lab8/ContentProviderLabSubmit/ContentProviderLab/PlaceViewActivity.java
@@ -1,112 +1,99 @@
-package course.labs.locationlab;
-
-import org.openintents.sensorsimulator.hardware.SensorManagerSimulator;
-import org.openintents.sensorsimulator.hardware.Sensor;
-import org.openintents.sensorsimulator.hardware.SensorEvent;
-import org.openintents.sensorsimulator.hardware.SensorEventListener;
-
-
+package course.labs.contentproviderlab;
 
 import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.ContentResolver;
 import android.content.Context;
-//import android.hardware.Sensor;
-//import android.hardware.SensorEvent;
-//import android.hardware.SensorEventListener;
-//import android.hardware.SensorManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ListView;
 import android.widget.Toast;
+import course.labs.contentproviderlab.provider.PlaceBadgesContract;
 
-public class PlaceViewActivity extends ListActivity implements LocationListener, SensorEventListener {
+
+public class PlaceViewActivity extends ListActivity implements
+		LocationListener, LoaderCallbacks<Cursor> {
 	private static final long FIVE_MINS = 5 * 60 * 1000;
-	private static final String TAG = "Lab-Location";
 
-	// Set to false if you don't have network access
+	private static String TAG = "Lab-ContentProvider";
+
+	// False if you don't have network access
 	public static boolean sHasNetwork = true;
 
-	private Location mLastLocationReading;
-	private PlaceViewAdapter mAdapter;
-	private LocationManager mLocationManager;
 	private boolean mMockLocationOn = false;
 
-	// default minimum time between new readings
+	// The last valid location reading
+	private Location mLastLocationReading;
+
+	// The ListView's adapter
+	// private PlaceViewAdapter mAdapter;
+	private PlaceViewAdapter mCursorAdapter;
+
+	// default minimum time between new location readings
 	private long mMinTime = 5000;
 
 	// default minimum distance between old and new readings.
 	private float mMinDistance = 1000.0f;
-	
-	//keep track och last shake update
-	private long mLastShakeUpdate = 0;
+
+	// Reference to the LocationManager
+	private LocationManager mLocationManager;
 
 	// A fake location provider used for testing
 	private MockLocationProvider mMockLocationProvider;
-	
-	private SensorManagerSimulator mSensorManager;
-	//private Sensor mAccelerometer;
-	
-	private View mFooterView;
-	
-	private float[] gravity ={.0f,.0f,.0f};
-	private float[] linear_acceleration ={.0f,.0f,.0f};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Set up the app's user interface. This class is a ListActivity, 
-		// so it has its own ListView. ListView's adapter should be a PlaceViewAdapter
+		if (!Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+			Toast.makeText(getApplicationContext(),
+					"External Storage is not available.", Toast.LENGTH_LONG)
+					.show();
+			finish();
+		}
 
 		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		ListView placesListView = getListView();
-
-		mSensorManager = SensorManagerSimulator.getSystemService(getApplication(),Context.SENSOR_SERVICE);
-		
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-	    .permitAll().build();
-	StrictMode.setThreadPolicy(policy);
-	mSensorManager = SensorManagerSimulator.getSystemService(this,
-	    SENSOR_SERVICE);
-	mSensorManager.connectSimulator();
-         //mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		
 		// TODO - add a footerView to the ListView
 		// You can use footer_view.xml to define the footer
-
 		
-		mFooterView = View.inflate(getApplicationContext(), R.layout.footer_view, null);
-		mFooterView.setClickable(false); //disable until position is acquired
-
+		View footerView = null;
+		footerView = View.inflate(getApplicationContext(), R.layout.footer_view, null);
+//		footerView.setClickable(false); //disable until position is acquired
 
 		// TODO - footerView must respond to user clicks, handling 3 cases:
 
-		// There is no current location - response is up to you. One good 
-		// solution is to disable the footerView until you have acquired a
+		// There is no current location - response is up to you. The best
+		// solution is to always disable the footerView until you have a
 		// location.
 
 		// There is a current location, but the user has already acquired a
-		// PlaceBadge for this location. In this case issue a Toast message with the text -
+		// PlaceBadge for this location - issue a Toast message with the text -
 		// "You already have this location badge." 
 		// Use the PlaceRecord class' intersects() method to determine whether 
-		// a PlaceBadge already exists for a given location.
+		// a PlaceBadge already exists for a given location
 
 		// There is a current location for which the user does not already have
 		// a PlaceBadge. In this case download the information needed to make a new
 		// PlaceBadge.
 
-		mFooterView.setOnClickListener(new OnClickListener() {
-
+		footerView.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View view) {
+
 				Log.i(TAG, "Entered footerView.OnClickListener.onClick()");
 
 
@@ -114,7 +101,7 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 					return;
 				
 				//if the user has already enterd this location
-				if(mAdapter.intersects(mLastLocationReading))
+				if(mCursorAdapter.intersects(mLastLocationReading))
 				{
 					Toast.makeText(getApplicationContext(), "You already have this location badge", Toast.LENGTH_SHORT).show();
 					return;
@@ -124,13 +111,19 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 				PlaceDownloaderTask pdt = new PlaceDownloaderTask(PlaceViewActivity.this, sHasNetwork);
 				pdt.execute(mLastLocationReading);
 			}
-
 		});
 
-		placesListView.addFooterView(mFooterView);
-		mAdapter = new PlaceViewAdapter(getApplicationContext());
-		setListAdapter(mAdapter);
+		getListView().addFooterView(footerView);
 
+		// TODO - Create and set empty PlaceViewAdapter
+//		ContentResolver cr = getApplicationContext().getContentResolver();
+//		Cursor c = cr.query(PlaceBadgesContract.BASE_URI, null,null, null, null);
+		mCursorAdapter =  new PlaceViewAdapter(getBaseContext(),null,0);
+		getListView().setAdapter(mCursorAdapter);
+
+		// TODO - Initialize the loader
+		getLoaderManager().initLoader(0, null, this);
+		
 	}
 
 	@Override
@@ -139,96 +132,92 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 
 		startMockLocationManager();
 
-		mLastLocationReading = null;
-		
 		// TODO - Check NETWORK_PROVIDER for an existing location reading.
 		// Only keep this last reading if it is fresh - less than 5 minutes old
 		boolean av = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 		Log.i(TAG, "Network provider available: " + av);
 		
+		
+		
+		
+		
+		
 		// TODO - register to receive location updates from NETWORK_PROVIDER
 		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, mMinTime, mMinDistance, this);
-//		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,0,this);
-				
+
 		Location loc = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		if(null != loc && ageInMilliseconds(loc) + FIVE_MINS > System.currentTimeMillis())
 			mLastLocationReading = loc;
 		
 		Log.i(TAG,"Setting footer clickable: " + (null != mLastLocationReading));
-		mFooterView.setClickable(null != mLastLocationReading);
-		
-		mSensorManager.registerListener(this, 
-				mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), 
-				SensorManagerSimulator.SENSOR_DELAY_FASTEST);
-//		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+//		footerView.setClickable(null != mLastLocationReading);
 		
 	}
 
 	@Override
 	protected void onPause() {
-		super.onPause();
 
 		// TODO - unregister for location updates
 		mLocationManager.removeUpdates(this);
 		shutdownMockLocationManager();
-		mSensorManager.unregisterListener(this);
+		super.onPause();
+
 	}
 
-	// Callback method used by PlaceDownloaderTask
 	public void addNewPlace(PlaceRecord place) {
-		Log.i(TAG, "Entered addNewPlace()");
 
 		// TODO - Attempt to add place to the adapter, considering the following cases
 
-		// The place is null. In this case issue a Toast message with the text
+		Log.i(TAG,"addNewPlace called");
+		// The place is null - issue a Toast message with the text
 		// "PlaceBadge could not be acquired"
 		// Do not add the PlaceBadge to the adapter
 		if(null == place){
 			Toast.makeText(getApplicationContext(), "PlaceBadge could not be acquired", Toast.LENGTH_SHORT).show();
 			return;
 		}
-
 		
-		// A PlaceBadge for this location already exists. In this case issue a Toast message
+		// A PlaceBadge for this location already exists - issue a Toast message
 		// with the text - "You already have this location badge." Use the PlaceRecord 
 		// class' intersects() method to determine whether a PlaceBadge already exists
 		// for a given location. Do not add the PlaceBadge to the adapter
-		if( mAdapter.intersects(place.getLocation())){
+		if( mCursorAdapter.intersects(place.getLocation())){
 			Toast.makeText(getApplicationContext(), "You already have this location badge", Toast.LENGTH_SHORT).show();
 			return;
 		}
-			
-		// The place has no country name. In this case issue a Toast message
+		
+		
+		// The place has no country name - issue a Toast message
 		// with the text - "There is no country at this location". 
 		// Do not add the PlaceBadge to the adapter
 		if("" == place.getCountryName()){
 			Toast.makeText(getApplicationContext(), "There is no country at this location", Toast.LENGTH_SHORT).show();
 			return;
 		}
-
+		
 		
 		// Otherwise - add the PlaceBadge to the adapter
-		mAdapter.add(place);
+		Log.i(TAG,"calling mCursorAdapter.add");
+		mCursorAdapter.add(place);
 
+		
 	}
 
 	// LocationListener methods
 	@Override
 	public void onLocationChanged(Location currentLocation) {
 
-		Log.i(TAG,"onLocationChanged called...");	
-		
-		// TODO - Update last location considering the following cases.
+		// TODO - Update location considering the following cases.
 		// 1) If there is no last location, set the last location to the current
 		// location.
 		// 2) If the current location is older than the last location, ignore
 		// the current location
 		// 3) If the current location is newer than the last locations, keep the
 		// current location.
-		
+
 		if(null == mLastLocationReading){
 			mLastLocationReading = currentLocation;
-			mFooterView.setClickable(true);
+//			footerView.setClickable(true);
 			Log.i(TAG,"Setting footer clickable in onLocationChanged");
 			return;
 		}
@@ -236,25 +225,65 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 		if(ageInMilliseconds(currentLocation) < ageInMilliseconds(mLastLocationReading)){
 			mLastLocationReading = currentLocation;
 		}
-			
+		
+		
+		
+		
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
 		// not implemented
-		Log.i(TAG, "provider disbled!!!");
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
 		// not implemented
-		Log.i(TAG, "provider enabled!!!");
-
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// not implemented
+	}
+
+	
+	// LoaderCallback methods
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+
+		String[] projection = {
+				PlaceBadgesContract._ID,
+				PlaceBadgesContract.FLAG_BITMAP_PATH,
+				PlaceBadgesContract.COUNTRY_NAME,
+				PlaceBadgesContract.PLACE_NAME,
+				PlaceBadgesContract.LAT,
+				PlaceBadgesContract.LON 
+				};
+		String select = "((" +  PlaceBadgesContract._ID + " NOT NULL))";
+		
+		CursorLoader cl = new CursorLoader(this, PlaceBadgesContract.CONTENT_URI,projection,select,null,null);
+		// TODO - Create a new CursorLoader and return it
+		return cl;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> newLoader, Cursor newCursor) {
+
+		
+		// TODO - Swap in the newCursor
+		mCursorAdapter.swapCursor(newCursor);
+	
+	
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> newLoader) {
+
+		
+		// TODO - swap in a null Cursor
+		mCursorAdapter.swapCursor(null);
+	
+	
 	}
 
 	// Returns age of location in milliseconds
@@ -273,7 +302,7 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.delete_badges:
-			mAdapter.removeAllViews();
+			mCursorAdapter.removeAllViews();
 			return true;
 		case R.id.place_one:
 			mMockLocationProvider.pushLocation(37.422, -122.084);
@@ -300,61 +329,5 @@ public class PlaceViewActivity extends ListActivity implements LocationListener,
 			mMockLocationProvider = new MockLocationProvider(
 					LocationManager.NETWORK_PROVIDER, this);
 		}
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
-//		Log.i(TAG,"Sensor changed!! " + event.type);
-//		if(event.type != Sensor.TYPE_ACCELEROMETER)
-//			return;
-		
-		
-		//check that enough time has passed since last update
-		if(System.currentTimeMillis() < mLastShakeUpdate + 5000)
-			return;
-		
-		
-		float[] values = event.values;
-		double x2 = values[0]*values[0];
-		double y2 = values[1]*values[1];
-		double z2 = values[2]*values[2];
-		
-		
-		
-		double magnitude = Math.sqrt(x2+y2+z2);
-		//only do something if magnitude is large enough
-		
-		
-		if(magnitude > 10.0 && mLastLocationReading != null){
-			//new location places
-			Log.i(TAG,"Shake magnitude: " + magnitude);
-			PlaceDownloaderTask pdt = new PlaceDownloaderTask(PlaceViewActivity.this, sHasNetwork);
-			pdt.execute(mLastLocationReading);
-		
-			mLastShakeUpdate = System.currentTimeMillis();
-		}
-			
-		
-//		final float alpha = 0.8f;
-//
-//        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-//        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-//        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-//		
-//        linear_acceleration[0] = event.values[0] - gravity[0];
-//        linear_acceleration[1] = event.values[1] - gravity[1];
-//        linear_acceleration[2] = event.values[2] - gravity[2];
-//        
-//        
-//		Log.i(TAG,"Sensor changed: " + linear_acceleration[0] + " " + linear_acceleration[1] + " " + linear_acceleration[2]);
-		
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-//		Log.i(TAG,"Sensor changed!!");
-		
 	}
 }
